1cd3e57c95fd13aec67e3da07f03f9300cfa1bc0
Erin Rugas <erinjohnrugas@gmail.com>
2023-03-17 15:24:37 +0800

    added auth routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,22 +1,28 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: '/login',
+      name: 'login',
+      component: () => import('@/views/auth/Login.vue')
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
+      path: '/register',
+      name: 'register',
+      component: () => import('@/views/auth/Register.vue')
+    },
+    {
+      path: '/forgot-password',
+      name: 'forgot-password',
+      component: () => import('@/views/auth/ForgotPassword.vue')
+    },
+    {
+      path: '/',
+      name: 'login',
+      component: () => import('@/views/auth/Login.vue')
+    },
   ]
 })
 
