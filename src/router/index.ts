9b179ff41af9b6a4ba39adcb43998026a9885e3c
Erin Rugas <erinjohnrugas@gmail.com>
2023-03-17 15:26:34 +0800

    added title

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,21 +4,33 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      meta: {
+        title: "Login"
+      },
       path: '/login',
       name: 'login',
       component: () => import('@/views/auth/Login.vue')
     },
     {
+      meta: {
+        title: "Register"
+      },
       path: '/register',
       name: 'register',
       component: () => import('@/views/auth/Register.vue')
     },
     {
+      meta: {
+        title: "Forgot Password"
+      },
       path: '/forgot-password',
       name: 'forgot-password',
       component: () => import('@/views/auth/ForgotPassword.vue')
     },
     {
+      meta: {
+        title: "Login"
+      },
       path: '/',
       name: 'login',
       component: () => import('@/views/auth/Login.vue')
