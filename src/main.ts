ca8fa521c5f3e1c69c0efc9a8a316a4b7e8004d2
Erin Rugas <erinjohnrugas@gmail.com>
2023-03-17 15:29:45 +0800

    implement title per page and change styles path

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,20 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-import './assets/main.css'
+import './css/main.css'
 
 const app = createApp(App)
 
 app.use(router)
 
 app.mount('#app')
+
+/* Default title tag */
+const defaultDocumentTitle = "Vue Auth Template";
+
+/* Set document title from route meta */
+router.afterEach((to) => {
+    document.title = to.meta?.title
+        ? `${defaultDocumentTitle} - ${to.meta.title}`
+        : defaultDocumentTitle;
+});
